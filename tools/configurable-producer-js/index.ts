854e7b5ba9cf510d707d16a10876fc0f71c9bc7f
Sylvain Leroy <contact@sylvainleroy.com>
2023-08-20 21:46:26 +0200

    Simple tool ready

--- tools/configurable-producer-js/index.ts
+++ tools/configurable-producer-js/index.ts
@@ -1,26 +1,84 @@
 /**
  * This tool can be use to send test messages to a RabbitMQ instance
  */
-import { AMQPClient, AMQPError } from '@cloudamqp/amqp-client'
+import { AMQPClient, AMQPError } from "@cloudamqp/amqp-client";
+import yargs from "yargs";
+import * as os from "os";
+// get host name
+const hostName = os.hostname();
+
+const args: any = yargs
+  .options({
+    hostname: {
+      type: "string",
+      demandOption: false,
+      alias: "u",
+      default: "localhost",
+    },
+    message: {
+      type: "string",
+      demandOption: false,
+      alias: "m",
+      default: `Helloworld from ${hostName}`,
+    },
+    exchange: {
+      type: "string",
+      demandOption: false,
+      alias: "x",
+      default: "default-exchange",
+    },
+    routingKey: { type: "string", demandOption: false, alias: "t" },
+    repeat: { type: "number", demandOption: false, alias: "r", default: 0 },
+  })
+  .help("h")
+  .describe("hostname", "RabbitMQ hostname")
+  .describe("message", "message to be sent")
+  .describe("exchange", "exchange to send the message")
+  .describe("routingKey", "routing key to use")
+  .command(
+    "[hostname] [message] [exchange]",
+    "Send a message or a repetition of messages to RabbitMQ"
+  )
+  .alias("h", "help").argv;
+
+console.log(args);
+
+function wait(milleseconds: number) {
+  return new Promise((resolve) => setTimeout(resolve, milleseconds));
+}
 
 async function run() {
   try {
-    const amqp = new AMQPClient("amqp://localhost")
-    const conn = await amqp.connect()
-    const ch = await conn.channel()
-    const q = await ch.queue()
-    const consumer = await q.subscribe({noAck: true}, async (msg) => {
-      console.log(msg.bodyToString())
-      await consumer.cancel()
-    })
-    await q.publish("Hello World", {deliveryMode: 2})
-    await consumer.wait() // will block until consumer is canceled or throw an error if server closed channel/connection
-    await conn.close()
+    if (!args.routingKey) {
+      args.routingKey = args.exchange;
+    }
+
+    const amqp = new AMQPClient(`amqp://${args.hostname}`);
+    const conn = await amqp.connect();
+    const ch = await conn.channel();
+    ch.exchangeDeclare(args.exchange, "fanout", {
+      passive: false,
+      durable: true,
+      autoDelete: false,
+      internal: false,
+    });
+
+    if (args.repeat == 0) {
+      console.log(`Publish message on ${args.exchange}`);
+      await ch.basicPublish(args.exchange, args.routingKey, args.message);
+    } else {
+      while (args.repeat > 0) {
+        console.log(`Publish message on ${args.exchange}`);
+        await wait(args.repeat);
+        await ch.basicPublish(args.exchange, args.routingKey, args.message);
+      }
+    }
+    await conn.close();
   } catch (e: any) {
-    console.error("ERROR", e)
-    e.connection.close()
-    setTimeout(run, 1000) // will try to reconnect in 1s
+    console.error("ERROR", e);
+    e.connection.close();
+    setTimeout(run, 1000); // will try to reconnect in 1s
   }
 }
 
-run()
+run();
