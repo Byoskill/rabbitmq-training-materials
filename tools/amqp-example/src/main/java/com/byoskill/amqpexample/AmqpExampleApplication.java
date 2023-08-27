e53b7d569ce3c984916a9853bb3237b31028e5f4
Sylvain Leroy <contact@sylvainleroy.com>
2023-08-27 22:24:59 +0200

    Spring boot AMQP

---
+++ tools/amqp-example/src/main/java/com/byoskill/amqpexample/AmqpExampleApplication.java
@@ -0,0 +1,13 @@
+package com.byoskill.amqpexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmqpExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmqpExampleApplication.class, args);
+	}
+
+}
---
+++ tools/amqp-example/src/test/java/com/byoskill/amqpexample/AmqpExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byoskill.amqpexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmqpExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
